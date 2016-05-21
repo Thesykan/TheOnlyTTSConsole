388770b79cc1c7203751ae9be660b3a199ad3fb7
David <Paxception@gmail.com>
2016-05-21 18:45:42 -0400

    Remove OAUTH.

--- SimpleConsole/GetAuthToken.cs
+++ SimpleConsole/GetAuthToken.cs
@@ -1,4 +1,4 @@
-﻿using DotNetOpenAuth.OAuth2;
+﻿//using DotNetOpenAuth.OAuth2;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,24 +9,24 @@ namespace SimpleConsole
 {
     public class GetAuthToken
     {
-        public void start(String p_userName, String p_password)
-        {
-            var authServer = new DotNetOpenAuth.OAuth2.AuthorizationServerDescription
-            {
-                AuthorizationEndpoint = new Uri(@"https://api.twitch.tv/kraken/oauth2/authorize"),
-            };
+        //public void start(String p_userName, String p_password)
+        //{
+        //    var authServer = new DotNetOpenAuth.OAuth2.AuthorizationServerDescription
+        //    {
+        //        AuthorizationEndpoint = new Uri(@"https://api.twitch.tv/kraken/oauth2/authorize"),
+        //    };
 
-            authServer.TokenEndpoint = new Uri(@"https://api.twitch.tv/kraken/oauth2/token");
+        //    authServer.TokenEndpoint = new Uri(@"https://api.twitch.tv/kraken/oauth2/token");
 
-            try
-            {
-                var client = new DotNetOpenAuth.OAuth2.UserAgentClient(authServer, p_userName, p_password);
-                OAuthUtilities.SplitScopes("chat");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-        }
+        //    try
+        //    {
+        //        var client = new DotNetOpenAuth.OAuth2.UserAgentClient(authServer, p_userName, p_password);
+        //        OAuthUtilities.SplitScopes("chat");
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(ex.ToString());
+        //    }
+        //}
     }
 }
